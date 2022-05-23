5d552a6a2d5d52378a68925ff399481ae4fe3718
Toan Tran <toantranct94@gmail.com>
2022-05-23 21:33:35 +0700

    Update script

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
-
-
 class BaseConfig(object):
-    nmi_configuration = ['E1', 'Q1']
+    nmi_configuration = ['E1Q1']
+    nmi_suffixes = ['E1', 'Q1']
     weathers = ['Heat', 'Snowy', 'Cloudy', 'Rainy', 'Lightning']
     regions = ['WA', 'NSW', 'QLD', 'SA', 'TAS', 'VIC']
-    intervel = 5
-    uom = 'kWh'
+    meter_type = ['Smart Meter', 'Standard Meter']
+    location_configuration = ['Acacia Avenue', 'Aberfeldy Street', 'Maibry Street-Wishart', 'Malcolm Street-Enoggera', 'Metro Street-Mcdowall', 'Metroplex Avenue-Murrarie', 'Peter Street-Clayfield', 'Deckerplace Huntingwood', 'Hills Street Garbutt', 'Church Street Toorak', 'Swinbourne Street Botany', 'Beach Road Avalon Airport', 'Concord Crescent Carrum Downs', 'Foorscray Road West Melbourne', 'Palmer Street Portsmith', 'Percy Auburn', 'Hedley Avenue Hendra', 'Riverview Place Murrarie', 'Pie Street-Aspley', 'Depot Street Maroonchydore', 'Nudgee Road Hendra', 'Pilinger Road', 'Pinkwood Street', 'Hills Street Garbutt',
+    'Alex Fisher Drive Burleigh', 'Paradise Road-Willawong', 'Goldsborough Road Pooraka', 'Grand Junction Road Wingfield',
+    'Tully Street-Keprra', 'Tumna Street-Ferny Grove', 'Union Street-Clayfield']
+    interval = 5
+    uom = ['kWh', 'kVArh']
     quality_methods = ['A', 'S', 'F', 'V']
-    number_of_generate_nmi = 1000
+    number_of_generate_nmi = 10
     number_of_generate_day = 30
     number_of_generate_files = 1
-    register_id = '001'
+    register_id = ['001', '002']
+    mdm_datastream_id = 'N1'
     meter_name = 'METSER_123'
     kwh_range = {
         0: [0, 30],
@@ -19,8 +23,8 @@ class BaseConfig(object):
     }
     row_100 = ['100' ,'NEM12', '200312221300', 'MDA1' ,'Ret1']
     row_400s = [
-        ['400', 1, 30, 'S'],
-        ['400', 31, 288, 'A'],
+        ['400', 1, 30, 'S14', '', ''],
+        ['400', 31, 288, 'A', '', ''],
     ]
     row_900 = [
         '900'
--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
+from decimal import BasicContext
 import random
 import csv
 import os
@@ -5,10 +6,15 @@ from datetime import datetime
 from config import BaseConfig
 from typing import List
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 def get_nmi_configuration() -> str:
     return random.choice(BaseConfig.nmi_configuration)
+
+
+def get_location(index) -> str:
+    return str(index+1) + ' ' + random.choice(BaseConfig.location_configuration)
 
 
 def get_weather() -> str:
@@ -31,7 +37,8 @@ def generate_nmi_name(index: int) -> str:
 
 def get_current_date(interval: int) -> str:
     now = datetime.now() + timedelta(days=interval)
-    return now.strftime('%Y%m%d')
+    four_month_ago_date = now - relativedelta(months=2)
+    return four_month_ago_date.strftime('%Y%m%d')
 
 
 def get_weight_by_weather(weather: str) -> float:
@@ -69,6 +76,7 @@ def get_temp_and_humidity(weather: str) -> float:
         humidity = 60
     return temp, humidity
 
+
 def get_weather_information() -> str:
     weather = get_weather()
     temp, humidity = get_temp_and_humidity(weather)
@@ -78,20 +86,38 @@ def get_weather_information() -> str:
 def calculate_kwhs(weather: str, kwh_range: List) -> List:
     weight = get_weight_by_weather(weather)
     kwhs = []
-    for _ in range(8):
+    for _ in range(96):
         kwhs.append(
             weight * round(random.uniform(kwh_range[0], kwh_range[1]), 3)
         )
     return kwhs
 
 
+def get_uom(nmi_suffix: str) -> str:
+    if nmi_suffix == 'Q1':
+        return BaseConfig.uom[1]
+    return BaseConfig.uom[0]
+
+
+def get_register_id(nmi_suffix: str) -> str:
+    if nmi_suffix == 'Q1':
+        return BaseConfig.register_id[1]
+    return BaseConfig.register_id[0]
+
+
+def get_meter_type() -> str:
+    return random.choice(BaseConfig.meter_type)
+
+
 def generate_dummy_data(index):
     '''
     Contact Alex or Tony to read more about the generate rules
     '''
-    path = os.path.join(os.getcwd(), f'nem5_{index}.csv')
+    path = os.path.join(os.getcwd(), f'NEM12#{index}1.csv')
     exist_weather = {}
     nmi_region = {}
+    nmi_location = {}
+    nmi_meter_type = {}
     with open(path, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(BaseConfig.row_100)
@@ -101,6 +127,8 @@ def generate_dummy_data(index):
                 nmi_name = generate_nmi_name(nmi_index)
                 nmi_configuration = get_nmi_configuration()
                 exist_region = nmi_region.get(nmi_name, '')
+                exist_location = nmi_location.get(nmi_name, '')
+                exist_meter_type = nmi_meter_type.get(nmi_name, '')
                 if not exist_region:
                     region = get_region()
                     nmi_region.update({
@@ -108,38 +136,55 @@ def generate_dummy_data(index):
                     })
                 else:
                     region = exist_region
-                row_200 = [
-                    '200', nmi_name, nmi_configuration, BaseConfig.register_id,
-                    nmi_configuration, BaseConfig.meter_name, BaseConfig.uom,
-                    BaseConfig.intervel, current_date, region
-                ]
-                row_300 = [
-                    '300', current_date
-                ]
-                for weather_index in range(3):
-                    key_weather = f'{region}_{weather_index}'
-                    if not exist_weather.get(key_weather, None):
-                        weather_info = get_weather_information()
-                        exist_weather.update({
-                            key_weather: weather_info
-                        })
-                    else:
-                        weather_info = exist_weather.get(key_weather, [])
-                    row_200.extend(weather_info)
-                    weather = weather_info[0]
-                    kwhs = calculate_kwhs(
-                        weather, BaseConfig.kwh_range.get(weather_index)
-                    )
-                    row_300.extend(kwhs)
-                writer.writerow(row_200)
-                quality = get_quality_method()
-                row_300.append(quality)
-                writer.writerow(row_300)
-                if quality == 'V':
-                    for row_400 in BaseConfig.row_400s:
-                        writer.writerow(row_400)
+                if not exist_location:
+                    location = get_location(nmi_index)
+                    nmi_location.update({
+                        nmi_name: location
+                    })
+                else:
+                    location = exist_location
+                if not exist_meter_type:
+                    meter_type = get_meter_type()
+                    nmi_meter_type.update({
+                        nmi_name: meter_type
+                    })
+                else:
+                    meter_type = exist_meter_type
+                for nmi_suffix in BaseConfig.nmi_suffixes:
+                    uom = get_uom(nmi_suffix)
+                    register_id = get_register_id(nmi_suffix)
+                    row_200 = [
+                        '200', nmi_name, nmi_configuration, register_id,
+                        nmi_suffix, BaseConfig.mdm_datastream_id, BaseConfig.meter_name,
+                        uom, BaseConfig.interval, current_date, region, location, meter_type
+                    ]
+                    row_300 = [
+                        '300', current_date
+                    ]
+                    for weather_index in range(3):
+                        key_weather = f'{region}_{weather_index}'
+                        if not exist_weather.get(key_weather, None):
+                            weather_info = get_weather_information()
+                            exist_weather.update({
+                                key_weather: weather_info
+                            })
+                        else:
+                            weather_info = exist_weather.get(key_weather, [])
+                        row_200.extend(weather_info)
+                        weather = weather_info[0]
+                        kwhs = calculate_kwhs(
+                            weather, BaseConfig.kwh_range.get(weather_index)
+                        )
+                        row_300.extend(kwhs)
+                    writer.writerow(row_200)
+                    quality = get_quality_method()
+                    row_300.append(quality)
+                    row_300 += ['', '', current_date, current_date]
+                    writer.writerow(row_300)
+                    if quality == 'V':
+                        for row_400 in BaseConfig.row_400s:
+                            writer.writerow(row_400)
         writer.writerow(BaseConfig.row_900)
-
 
 
 if __name__ == "__main__":
